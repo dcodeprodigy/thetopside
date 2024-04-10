172588c0f9be0798549617089d31f000e3003d97
Joseph Nwodo <128279131+dcodeprodigy@users.noreply.github.com>
2024-04-10 15:22:18 -0500

    Add files via upload

--- general-js/main.js
+++ general-js/main.js
@@ -70,21 +70,25 @@ newsletterLink.addEventListener("click", ()=>{
 
 
 // For adjusting the width of the top guage which indicates the level the user has scrolled to
-let grossBodyHeight;
-let currentScrollHeight;
-let netBodyHeight;
-let percentageOfPageScrolled;
+// let grossBodyHeight;
+// let currentScrollHeight;
+// let netBodyHeight;
+// let percentageOfPageScrolled;
 
-window.addEventListener("scroll", ()=>{
-    grossBodyHeight = Number(body.scrollHeight); // 6405  8386 
-    // This gives the total scrollable height of the body element.
+// window.addEventListener("scroll", ()=>{
+//     grossBodyHeight = Number(body.scrollHeight); // 6405  8386 
+//     // This gives the total scrollable height of the body element.
 
-    currentScrollHeight = Number(window.scrollY); // 5764  7745 => These figures were used to get a constant(641), regardless of the height of the window, that 641px is constant.
-    // This gives the current scroll height. It updates as we keep scrolling.
+//     currentScrollHeight = Number(window.scrollY); // 5764  7745 => These figures were used to get a constant(641), regardless of the height of the window, that 641px is constant.
+//     // This gives the current scroll height. It updates as we keep scrolling.
 
-    netBodyHeight = Number(grossBodyHeight - 641) ; // This is the height after we subtract the constant value of 641 pixel. This(641) is the descripancy between the body's scrollable height and the current scroll height. We did this in order to be able to calculate the percentage of the height we have scrolled to.
 
-    percentageOfPageScrolled = (currentScrollHeight/netBodyHeight) * 100;
-    guage.style.width = `${percentageOfPageScrolled}%`;
+//     // netBodyHeight = grossBodyHeight - (grossBodyHeight - currentScrollHeight); // This is the height after we subtract the constant value of 641 pixel. This(641) is the descripancy between the body's scrollable height and the current scroll height. We did this in order to be able to calculate the percentage of the height we have scrolled to.
 
-})
+//     netBodyHeight = grossBodyHeight - currentScrollHeight;
+//     // percentageOfPageScrolled = (currentScrollHeight/netBodyHeight) * 100;
+//     // percentageOfPageScrolled = ((currentScrollHeight + netBodyHeight)/grossBodyHeight)  * 100
+//     percentageOfPageScrolled = ((currentScrollHeight + netBodyHeight)/grossBodyHeight) * 100;
+//     guage.style.width = `${percentageOfPageScrolled}%`;
+//     console.log(`gross body height ${grossBodyHeight}, current height ${currentScrollHeight}, percentage scrolled ${percentageOfPageScrolled}, net height diff ${netBodyHeight}`);
+// })
