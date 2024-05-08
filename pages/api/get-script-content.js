a98acabdfada3b0f18ed90611e43c6df85f2aabd
Joseph Nwodo <128279131+dcodeprodigy@users.noreply.github.com>
2024-05-08 14:15:02 +0100

    Update get-script-content.js

--- pages/api/get-script-content.js
+++ pages/api/get-script-content.js
@@ -2,7 +2,7 @@ import { fetch } from 'node-fetch';
 
 export default async function handler(req, res) {
     // Replace with your Google Apps Script Web App URL
-    const scriptUrl = 'https://script.google.com/macros/s/...'; 
+    const scriptUrl = 'https://script.google.com/macros/s/AKfycbzcA60vtPiYMBc2pVwmjCFGbg74dI11PqkrwWJabOKfE3pXeyHsx4weCUUaORcqE8_8vw/exec'; 
 
     const response = await fetch(scriptUrl);
     const scriptContent = await response.text(); // Assumes the script returns plain HTML
